73dd1750b450d366b91d5eab32fa6b79a3d71f80
ColonelFox <dacanay.aj.06@gmail.com>
2022-09-24 09:42:13 +0800

    First Commit

---
+++ HandsOnExerciseODL.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hands on Exercises ODL</title>
+</head>
+<body>
+
+<h2>PHP Array Exercise 1</h2>
+<?php
+	$weather=array("rain","sunshine","clouds","hail","sleet","snow","wind");
+	echo "We've seen all kinds of weather this month. At the beginning of the month, we had ".$weather[5]." and ".$weather[6].". <br>Then came ".$weather[1]." with a few ".$weather[2]." and some ".$weather[0].". At least we didn't get any ".$weather[3]." or ".$weather[4].".";
+?>
+
+<h2>PHP Array Exercise 2</h2>
+<?php
+	$cities=array("Tokyo","Mexico City","New York City","Mumbai","Seoul","Shanghai","Lagos","Buenos Aires","Cairo","London");
+	echo "<b> Unsorted List of the Countries: </b><br>";
+	for ($i=0; $i < count($cities); $i++) { 
+		if ($i==count($cities)-1) {
+			echo $cities[$i].".";
+		}else{
+			echo $cities[$i].", ";
+		}
+	}
+	echo "<br> <br> <b>Sorted list of the Countries: </b><br>";
+	sort($cities);
+	for ($i=0; $i < count($cities); $i++) { 
+		if ($i==count($cities)-1) {
+			echo $cities[$i].".";
+		}else{
+			echo $cities[$i].", ";
+		}
+	}
+?>
+
+</body>
+</html>
