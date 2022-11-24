3d79c883e02b6daff85f11bd349eba65aace5cd0
Andrew James Dacanay <72633020+ColonelFox@users.noreply.github.com>
2022-11-24 19:02:29 +0800

    phpcrud
    phpcrud

---
+++ phpcrud/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer()?>
---
+++ phpcrud/read.php
@@ -0,0 +1,66 @@
+<?php
+include 'functions.php';
+// Connect to MySQL database
+$pdo = pdo_connect_mysql();
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
+
+// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
+?>
+
+<?=template_header('Read')?>
+
+<div class="content read">
+	<h2>Read Contacts</h2>
+	<a href="create.php" class="create-contact">Create Contact</a>
+	<table>
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Title</td>
+                <td>Created</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $contact): ?>
+            <tr>
+                <td><?=$contact['id']?></td>
+                <td><?=$contact['name']?></td>
+                <td><?=$contact['email']?></td>
+                <td><?=$contact['phone']?></td>
+                <td><?=$contact['title']?></td>
+                <td><?=$contact['created']?></td>
+                <td class="actions">
+                    <a href="update.php?id=<?=$contact['id']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                    <a href="delete.php?id=<?=$contact['id']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+	<div class="pagination">
+		<?php if ($page > 1): ?>
+		<a href="read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+		<?php endif; ?>
+		<?php if ($page*$records_per_page < $num_contacts): ?>
+		<a href="read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+		<?php endif; ?>
+	</div>
+</div>
+
+<?=template_footer()?>
